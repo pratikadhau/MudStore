780d683b37d5ffc42c4006a35e7852de85df8e52
vinitagaikwad <gaikwadvinita29@gmail.com>
2014-05-29 10:09:58 +0530

    Integrating Jersey Rest with Spring

--- src/main/java/com/mudstore/rest/controller/ProductRestController.java
+++ src/main/java/com/mudstore/rest/controller/ProductRestController.java
@@ -1,5 +1,7 @@
 package com.mudstore.rest.controller;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -13,14 +15,21 @@ import com.mudstore.model.Product;
 import com.mudstore.service.ProductService;
 
 @Component
-@Path("/product/{id}")
-public class ProductController {
+@Path("/product")
+public class ProductRestController {
 	@Autowired
 	ProductService productService;
 	
 	@GET
 	@Produces(MediaType.TEXT_XML)
+	@Path("{id}")
 	public Product getProduct(@PathParam("id") Integer productId){
 		return productService.getProduct(productId);
+	}
+	
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public List<Product> getAllProduct(){
+		return productService.getAllProducts();
 	}
 }
