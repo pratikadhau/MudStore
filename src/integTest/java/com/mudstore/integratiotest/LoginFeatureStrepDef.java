611ee68003f9865bfe457e3706ab9b1e4696a749
Pratik_Adhau <Pratik.Adhau@cognizant.com>
2014-06-13 17:32:27 +0530

    gradle script for running functional test

--- src/integTest/java/com/mudstore/integratiotest/LoginFeatureStrepDef.java
+++ src/integTest/java/com/mudstore/integratiotest/LoginFeatureStrepDef.java
@@ -13,10 +13,12 @@ import cucumber.api.java.en.When;
 public class LoginFeatureStrepDef {
 
 	WebDriver driver = new ChromeDriver();
+	private String username=null;
 	static {System.setProperty("webdriver.chrome.driver", "/home/lt013962/Downloads/chromedriver");}
 	
 	@When("^I entered username (\\S+) and password (\\S+).")
 	public void I_enter_username(String username, String password) throws InterruptedException{
+		this.username=username;
 		driver.get("http://localhost:8080/MudStore");
 		WebElement usernameElement = driver.findElement(By.id("username"));
 		usernameElement.sendKeys(username);
@@ -32,5 +34,12 @@ public class LoginFeatureStrepDef {
 	public void failed_login(){
 		WebElement erroeElement = driver.findElement(By.id("loginerror"));
 		Assert.assertEquals("Invalid password", erroeElement.getText());
+	}
+	
+	@Then("^I should be able to see home page")
+	public void successfull_login(){
+		Assert.assertEquals("Mudstore",driver.getTitle());
+		WebElement welcomeMessage = driver.findElement(By.id("MudStoreWelcome"));
+		Assert.assertEquals("Welcome "+this.username, welcomeMessage);
 	}
 }
--- src/integTest/resources/features/login.feature
+++ src/integTest/resources/features/login.feature
@@ -4,3 +4,6 @@ Scenario: Login_should_not_be_allowed
     When I entered username pratik1 and password adhau.
     Then I should not allowed to login
     
+Scenario: Successfull login should land up in home page
+    When I entered username pratik and password adhau.
+    Then I should be able to see home page
